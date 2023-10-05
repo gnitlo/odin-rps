84a2757fd72abcaf4f0f1f997fe180a6b7a6b703
Erik André Paulsen <130229403+gnitlo@users.noreply.github.com>
2023-10-05 22:37:31 +0200

    Add 5 rounds, point counter and display result
    The game now plays 5 times in a row and displays result of the round
    in the console log. It also stores wins, losses and draws in variables
    displayed in the log after the game is over.

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/*
-Function getComputerChoice
-    Output random: rock, paper scissors
-
-Function playGame
-    Input computerSelection
-    Input playerSelection
-    Run toUpperCase() for playerSelection
-    Return string "You win! X beats Y", "You lose! X beats Y"
-*/
 function getComputerChoice(){
     let randomChoice = ["ROCK","PAPER","SCISSOR"];
     const randomIndex = Math.floor(Math.random() * randomChoice.length);
@@ -23,45 +13,60 @@ function getPlayerChoice(){
     }
     else {
         alert ("Invalid choice")
+        playerLoss++;
         return
     }
-
 }
 function playRound(computerChoice,playerChoice){
     if (playerChoice == "PAPER"){
         if (computerChoice == "PAPER"){
             console.log("Draw! " + playerChoice + " draws against " + computerChoice);
+            playerDraw++;
         }
         else if (computerChoice == "ROCK"){
             console.log("You win! " + playerChoice + " wins against " + computerChoice);
+            playerWin++;
         }
         else if (computerChoice == "SCISSOR"){
             console.log("You lose! " + playerChoice + " loses against " + computerChoice);
+            playerLoss++;
         }
     }
     if (playerChoice == "ROCK"){
         if (computerChoice == "PAPER"){
             console.log("You lose! " + playerChoice + " loses against " + computerChoice);
+            playerLoss++;
         }
         else if (computerChoice == "ROCK"){
             console.log("Draw! " + playerChoice + " draws against " + computerChoice);
+            playerDraw++;
         }
         else if (computerChoice == "SCISSOR"){
             console.log("You win! " + playerChoice + " wins against " + computerChoice);
+            playerWin++;
         }
     }
     if (playerChoice == "SCISSOR"){
         if (computerChoice == "PAPER"){
             console.log("You win! " + playerChoice + " wins against " + computerChoice);
+            playerWin++;
         }
         else if (computerChoice == "ROCK"){
             console.log("You lose! " + playerChoice + " loses against " + computerChoice);
+            playerLoss++;
         }
         else if (computerChoice == "SCISSOR"){
             console.log("Draw! " + playerChoice + " draws against " + computerChoice);
+            playerDraw++;
         }
     }
 }
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerChoice();
-playRound(computerChoice,playerChoice);
+let playerWin = 0;
+let playerLoss = 0;
+let playerDraw = 0;
+for (let i = 0; i < 5; i++){
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    playRound(computerChoice,playerChoice);
+}
+console.log(`Game over! You won ${playerWin} times, you lost ${playerLoss} times and the round ended in a draw ${playerDraw} times.`)
